4eed6797a22241bdaec4aeb255795a672382252e
Roman <elio.sarpi@gmail.com>
2021-08-08 14:25:43 +1000

    🚑 | Fix Roll Command

--- commands/fun/roll.js
+++ commands/fun/roll.js
@@ -4,7 +4,9 @@ module.exports = {
 	cooldown: '2',
 	args: true,
 	async execute(client, message, args) {
-		const number = Math.floor(Math.random() * (args[0] - args[1])) + args[1];
+		if (!args[1]) return message.reply('I Need A Second Number!');
+		if (!Number.isInteger(args[0]) || !Number.isInteger(args[1])) return message.reply('At least one of your inputs were not a number!');
+		const number = Math.floor(Math.random() * (Math.floor(args[0]) - Math.ceil(args[1]))) + Math.ceil(args[1]);
 		message.channel.send(`It's ${number}!`);
 	},
 };
