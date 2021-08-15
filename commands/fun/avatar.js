9af38669f2c345abd6a5d64ef9a2fefd3d0684a7
Roman <elio.sarpi@gmail.com>
2021-08-15 20:03:39 +1000

    🚑 | Fix Avatar Command

--- commands/fun/avatar.js
+++ commands/fun/avatar.js
@@ -2,22 +2,23 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
 	name: 'avatar',
 	description: 'Get the avatar of a user',
+	usage: 'avatar [user]',
 	cooldown: 5,
 	aliases:[ 'av', 'pfp' ],
-	execute(message, args) {
+	async execute(client, message, args) {
 		let embed = '';
 		if (!args[0]) {
 			embed = new MessageEmbed()
 				.setTitle(`Avatar of ${message.author.username}`)
 				.setColor('BLUE')
-				.setImage(message.author.avatarURL({ dynamic:true }) + '?size=1024')
-				.setURL(message.author.avatarURL({ dynamic:true }) + '?size=1024');
+				.setImage(message.author.displayAvatarURL() + '?size=1024')
+				.setURL(message.author.displayAvatarURL() + '?size=1024');
 		} else {
 			embed = new MessageEmbed()
 				.setTitle(`Avatar of ${message.mentions.users.first().username}`)
 				.setColor('BLUE')
-				.setImage(message.mentions.users.first().avatarURL({ dynamic:true }) + '?size=1024')
-				.setURL(message.mentions.users.first().avatarURL({ dynamic:true }) + '?size=1024');
+				.setImage(message.mentions.users.first().displayAvatarURL() + '?size=1024')
+				.setURL(message.mentions.users.first().displayAvatarURL() + '?size=1024');
 		}
 		message.channel.send({ embeds: [embed] });
 	},
