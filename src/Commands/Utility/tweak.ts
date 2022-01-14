8080d75d1d6b46ff0c64423df7df87181792948e
CallMeEcho <romanbarrettsarpi@pm.me>
2022-01-14 22:02:51 +1100

    🐛 | Fix A Bugfix

--- src/Commands/Utility/tweak.ts
+++ src/Commands/Utility/tweak.ts
@@ -44,12 +44,12 @@ const Tweak: Command = {
 
     let color = null
 
-    if (!res.data[0].packageIcon.startsWith('http:') || !res.data[0].packageIcon.startsWith('https:')) {
-      color = '#fccc04'
-      res.data[0].packageIcon = undefined
-    } else {
+    if (res.data[0].packageIcon.startsWith('http:') || res.data[0].packageIcon.startsWith('https:')) {
       color = await Vibrant.from(res.data[0].packageIcon || 'https://repo.packix.com/api/Packages/60bfb71987ca62001c6585e6/icon/download?size=medium&hash=2').getPalette()
       color = color.Vibrant.hex
+    } else {
+      color = '#fccc04'
+      res.data[0].packageIcon = undefined
     }
     const row = new MessageActionRow()
       .addComponents(
